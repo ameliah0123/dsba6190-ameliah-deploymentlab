da8dd3c98bf89bd7e8db6e1246813f6181c0707b
ameliah0123 <aho3@charlotte.edu>
2025-11-19 17:54:45 -0500

    Add terraform code to repo

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "aho3"
+  default     = "ameliah"
 }
 
 variable "class_name" {
