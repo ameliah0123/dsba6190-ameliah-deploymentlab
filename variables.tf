f714c97761755d1eff849e32daf82f1065c318fa
ameliah0123 <aho3@charlotte.edu>
2025-11-19 17:50:34 -0500

    Add terraform files

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment" {
 variable "student_name" {
   description = "Application Name"
   type        = string
-  default     = "ameliah"
+  default     = "aho3"
 }
 
 variable "class_name" {
