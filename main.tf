36bc866859d49d9f1e8e1887f1099dc1ce3a1e79
ameliah0123 <aho3@charlotte.edu>
2025-11-19 20:05:08 -0500

    Add terraform code to repo

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "random_integer" "deployment_id_suffix" {
   max = 999
 }
 
-//Resource Group
+// Resource Group
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.class_name}-${var.student_name}-eastus-${var.environment}-${random_integer.deployment_id_suffix.result}"
