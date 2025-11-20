383e399f100c018baf69b81ddfcb0add3725bfbd
ameliah0123 <aho3@charlotte.edu>
2025-11-19 19:31:46 -0500

    Add terraform files

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ resource "azurerm_mssql_database" "db" {
   collation   = "SQL_Latin1_General_CP1_CI_AS"
   max_size_gb = 1
   read_scale  = false
-  sku_name    = "Basic"
+  sku_name    = "Standard"
 }
 
 // Storage Account
